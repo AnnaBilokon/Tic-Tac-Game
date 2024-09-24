9cfd4061b71f5fa2efa45a81a9ec3f6ac9ede6fc
Anna-Bilokon <anna.bilokon@illuminum.se>
2024-09-24 13:43:45 +0200

    game logic code

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,65 @@
 import { useState } from 'react'
 import Player from './components/Player'
 import GameBoard from './components/GameBoard'
+import Log from './components/Log'
+import { WINNING_COMBINATIONS } from './winning-combinations'
+import GameOver from './components/GameOver'
+
+const initialGameBoard = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null],
+]
+
+function deriveActivePlayer(gameTurn) {
+  let currentPlayer = 'X'
+
+  if (gameTurn.length > 0 && gameTurn[0].player === 'X') {
+    currentPlayer = 'O'
+  }
+  return currentPlayer
+}
 
 function App() {
-  const [activePlayer, setActivePlayer] = useState('X')
+  const [gameTurn, setGameTurn] = useState([])
+  const activePlayer = deriveActivePlayer(gameTurn)
 
-  function handleSelectSquare() {
-    setActivePlayer((curActivePlayer) => (curActivePlayer === 'X' ? 'O' : 'X'))
+  let gameBoard = initialGameBoard
+
+  for (const turn of gameTurn) {
+    const { square, player } = turn
+    const { row, col } = square
+
+    gameBoard[row][col] = player
+  }
+  let winner = null
+  for (const combination of WINNING_COMBINATIONS) {
+    const firstSquareSymbol =
+      gameBoard[combination[0].row][combination[0].column]
+    const secondSquareSymbol =
+      gameBoard[combination[1].row][combination[1].column]
+    const thirdSquareSymbol =
+      gameBoard[combination[2].row][combination[2].column]
+
+    if (
+      firstSquareSymbol &&
+      firstSquareSymbol === secondSquareSymbol &&
+      firstSquareSymbol === thirdSquareSymbol
+    ) {
+      winner = firstSquareSymbol
+    }
+  }
+
+  const hasDraw = gameTurn.length === 9 && !winner
+  function handleSelectSquare(rowIndex, colIndex) {
+    setGameTurn((prevTurn) => {
+      const currentPlayer = deriveActivePlayer(prevTurn)
+      const updatedTurn = [
+        { square: { row: rowIndex, col: colIndex }, player: currentPlayer },
+        ...prevTurn,
+      ]
+      return updatedTurn
+    })
   }
   return (
     <main>
@@ -23,11 +76,10 @@ function App() {
             isActive={activePlayer === 'O'}
           />
         </ol>
-        <GameBoard
-          onSelectSquare={handleSelectSquare}
-          activePlayerSymbol={activePlayer}
-        />
+        {(winner || hasDraw) && <GameOver winner={winner} />}
+        <GameBoard onSelectSquare={handleSelectSquare} board={gameBoard} />
       </div>
+      <Log turns={gameTurn} />
     </main>
   )
 }
